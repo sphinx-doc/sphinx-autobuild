e6d54848a41c8de89a1bebb7c062b85294d5e1f3
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2013-12-25 19:21:14 +0100

    Version bump and NEWS updates.

--- sphinx_autobuild.py
+++ sphinx_autobuild.py
@@ -18,7 +18,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 
 class _WatchdogHandler(FileSystemEventHandler):
