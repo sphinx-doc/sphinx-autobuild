d8df29faabfeb80d9e15a6cd4123626c3649f3b0
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-08-20 13:01:39 +0530

    Add nox -s test

--- noxfile.py
+++ noxfile.py
@@ -7,3 +7,16 @@ import nox
 def lint(session):
     session.install("pre-commit")
     session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
+@nox.session(python=["3.6", "3.7", "3.8"])
+def test(session):
+    session.install(".[test]")
+    # fmt: off
+    default_args = [
+        "--cov-report", "term",
+        "--cov", "sphinx_autobuild",
+        "sphinx_autobuild",
+    ]
+    # fmt: on
+    session.run("pytest", *(session.posargs or default_args))
