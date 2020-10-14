7208ba91afe0c5e99cac4065a6e6f888a1863172
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-10-15 04:07:44 +0530

    Rework build output handling
    - Print the exact commands being executed.
    - Let the subprocess directly handle I/O streams.
     (no more box around output)
    - Add colors, with colorama, to make it easier to identify points of
      interest -- like the file that triggered rebuild or command executed.
    - Present helpful error message when the subprocesses fail.
    - Skip the main sphinx build, if pre-build commands fail.

--- src/sphinx_autobuild/utils.py
+++ src/sphinx_autobuild/utils.py
@@ -1,17 +1,7 @@
 """Generic utilities."""
 
-import os
 import socket
-import subprocess
-import sys
 from contextlib import closing
-
-try:
-    import pty
-except ImportError:
-    have_pty = False
-else:
-    have_pty = True
 
 
 def find_free_port():
@@ -23,47 +13,3 @@ def find_free_port():
         s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
-
-
-def run_with_surrounding_separators(args, *, heading, include_footer=True):
-    """Run a subprocess with the output surrounded by a box.
-
-    Looks like::
-
-        +---------- heading ----------
-        | first line of output
-        | second line of output
-        +-----------------------------
-    """
-    separator_width = 80
-    header = "+" + f"-- {heading} --".center(separator_width, "-")
-    footer = "+" + "-" * separator_width
-
-    sys.stdout.write(header + "\n")
-
-    if have_pty:
-        parent, child = pty.openpty()
-        stdout = os.fdopen(parent)
-        subprocess.Popen(args, stdout=child)
-        os.close(child)
-    else:
-        stdout = subprocess.Popen(
-            args, stdout=subprocess.PIPE, universal_newlines=True
-        ).stdout
-
-    try:
-        while 1:
-            line = stdout.readline()
-            if not line:
-                break
-            sys.stdout.write("| ")
-            sys.stdout.write(line.rstrip())
-            sys.stdout.write("\n")
-    except IOError:
-        pass
-    finally:
-        if not have_pty:
-            stdout.close()
-
-    if include_footer:
-        sys.stdout.write(footer + "\n")
