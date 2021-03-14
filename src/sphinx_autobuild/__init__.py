1eb7d7746b773f515de64f1bd83c14bccca8a1ef
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-03-14 13:43:08 +0000

    Bump for release

--- src/sphinx_autobuild/__init__.py
+++ src/sphinx_autobuild/__init__.py
@@ -1,4 +1,4 @@
 """Rebuild Sphinx documentation on changes, with live-reload in the browser."""
 
-__version__ = "2020.09.02.dev0"
+__version__ = "2021.03.14"
 __url__ = "https://github.com/GaretJax/sphinx-autobuild"
