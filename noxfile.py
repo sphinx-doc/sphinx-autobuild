631245dc954711dab3bf2652830b82447c2eb177
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-01-30 21:41:59 +0000

    Simplify noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -3,22 +3,6 @@
 import nox
 
 
-def _install_this_editable(session, *, extras=None):
-    if extras is None:
-        extras = []
-
-    session.install("flit")
-    session.run(
-        "flit",
-        "install",
-        "-s",
-        "--deps=production",
-        "--extras",
-        ",".join(extras),
-        silent=True,
-    )
-
-
 @nox.session(reuse_venv=True)
 def lint(session):
     session.install("pre-commit")
@@ -27,23 +11,20 @@ def lint(session):
 
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def test(session):
-    _install_this_editable(session, extras=["test"])
-
-    default_args = ["--cov-report", "term", "--cov", "sphinx_autobuild"]
-    args = session.posargs or default_args
-
+    session.install("-e", ".[test]", silent=True)
+    args = session.posargs or ("--cov-report", "term", "--cov", "sphinx_autobuild")
     session.run("pytest", *args)
 
 
 @nox.session(reuse_venv=True)
 def docs(session):
-    _install_this_editable(session, extras=["docs"])
+    session.install("-e", ".", silent=True)
     session.run("sphinx-build", "-b", "html", "docs/", "build/docs")
 
 
 @nox.session(name="docs-live", reuse_venv=True)
 def docs_live(session):
-    _install_this_editable(session, extras=["docs"])
+    session.install("-e", ".", silent=True)
     session.run(
         "sphinx-autobuild", "-b", "html", "docs/", "build/docs", *session.posargs
     )
