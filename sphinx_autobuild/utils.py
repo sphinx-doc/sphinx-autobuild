df324331a82084b564168f5781be6afe3fac987c
Adam Turner <9087854+aa-turner@users.noreply.github.com>
2024-01-30 19:24:42 +0000

    Remove redundant ``contextlib.closing``

--- sphinx_autobuild/utils.py
+++ sphinx_autobuild/utils.py
@@ -1,7 +1,6 @@
 """Generic utilities."""
 
 import socket
-from contextlib import closing
 
 
 def find_free_port():
@@ -9,7 +8,7 @@ def find_free_port():
 
     Shout-out to https://stackoverflow.com/a/45690594/1931274!
     """
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
