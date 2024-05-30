sphinx-doc/sphinx-autobuild

commit d57f1173a8a5d95565448106b5fba8ce8967d91c

Call Sphinx's build directly

sphinx_autobuild/build.py
@@ -2,9 +2,10 @@
 
 import shlex
 import subprocess
-import sys
 
 from colorama import Fore, Style
+# This isn't public API, but we want to avoid a subprocess call
+from sphinx.cmd.build import build_main
 
 
 def _log(text, *, colour):
@@ -31,8 +32,6 @@ class Builder:
     def __call__(self):
         """Generate the documentation using ``sphinx``."""
 
-        sphinx_command = [sys.executable, "-m", "sphinx"] + self.sphinx_args
-
         if self.watcher.filepath:
             show(context=f"Detected change: {self.watcher.filepath}")
 
@@ -40,24 +39,30 @@ class Builder:
             for command in self.pre_build_commands:
                 show(context="pre-build", command=command)
                 subprocess.run(command, check=True)
-
-            show(command=["sphinx-build"] + self.sphinx_args)
-            subprocess.run(sphinx_command, check=True)
         except subprocess.CalledProcessError as e:
-            self.cmd_exit(e.returncode)
-        finally:
-            # We present this information, so that the user does not need to keep track
-            # of the port being used. It is presented by livereload when starting the
-            # server, so don't present it in the initial build.
-            if self.watcher.filepath:
-                show(context=f"Serving on {self.uri}")
+            print(f"Pre-build command exited with exit code: {e.returncode}")
+            print(
+                "Please fix the cause of the error above or press Ctrl+C to stop the "
+                "server."
+            )
+            raise
 
-    @staticmethod
-    def cmd_exit(return_code):
-        print(f"Command exited with exit code: {return_code}")
-        print(
-            "The server will continue serving the build folder, but the contents "
-            "being served are no longer in sync with the documentation sources. "
-            "Please fix the cause of the error above or press Ctrl+C to stop the "
-            "server."
-        )
+        show(command=["sphinx-build"] + self.sphinx_args)
+
+        # NOTE:
+        # sphinx.cmd.build.build_main is not considered to be public API,
+        # but as this is a first-party project, we can cheat a little bit.
+        return_code = build_main(self.sphinx_args)
+        if return_code:
+            print(f"Sphinx exited with exit code: {return_code}")
+            print(
+                "The server will continue serving the build folder, but the contents "
+                "being served are no longer in sync with the documentation sources. "
+                "Please fix the cause of the error above or press Ctrl+C to stop the "
+                "server."
+            )
+        # We present this information, so that the user does not need to keep track
+        # of the port being used. It is presented by livereload when starting the
+        # server, so don't present it in the initial build.
+        if self.watcher.filepath:
+            show(context=f"Serving on {self.uri}")

commit f440a7415d7f5e58bac22ec9b549abdddff2e210

[fix] interaction with autodoc plugin / sphinx-build in subprocess
To update changes to the doc strings (autodoc) in the source code in the HTML
output, the sphinx-build must not run in the same process as the server /
sphinx-build must run in a subprocess so that changed source code is
re-imported.

This issue was introduced in commit d57f117 / a commit without a PR & review.

Related: https://github.com/sphinx-doc/sphinx-autobuild/issues/31#issuecomment-111232175
Closes: https://github.com/sphinx-doc/sphinx-autobuild/issues/148

Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

sphinx_autobuild/build.py
@@ -2,9 +2,6 @@
 
 import subprocess
 
-# This isn't public API, but we want to avoid a subprocess call
-from sphinx.cmd.build import build_main
-
 from sphinx_autobuild.utils import show
 
 
@@ -31,13 +28,11 @@ class Builder:
             )
             raise
 
-        show(command=["sphinx-build"] + self.sphinx_args)
-
-        # NOTE:
-        # sphinx.cmd.build.build_main is not considered to be public API,
-        # but as this is a first-party project, we can cheat a little bit.
-        if return_code := build_main(self.sphinx_args):
-            print(f"Sphinx exited with exit code: {return_code}")
+        try:
+            show(command=["sphinx-build"] + self.sphinx_args)
+            subprocess.run(["sphinx-build"] + self.sphinx_args, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Sphinx exited with exit code: {e.returncode}")
             print(
                 "The server will continue serving the build folder, but the contents "
                 "being served are no longer in sync with the documentation sources. "