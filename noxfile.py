23c16b0271363a561db87da81214805e4567d736
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-09-02 21:42:45 +0300

    Test Python 3.13 (#169)

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ def lint(session):
     session.run("pre-commit", "run", "--all-files", *session.posargs)
 
 
-@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def test(session):
     session.install("-e", ".[test]", silent=True)
     session.run("pytest", *session.posargs)
